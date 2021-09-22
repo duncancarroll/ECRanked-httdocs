0fe94246de97f15d191483b0c7882a7a2bcb8afe
BiffBish <42410516+BiffBish@users.noreply.github.com>
2021-09-22 09:46:49 -0400

    Update get_skim.php

--- api/get_skim.php
+++ api/get_skim.php
@@ -73,12 +73,12 @@
 
 
 ?>
-<title><?php echo htmlspecialchars($session_id, ENT_QUOTES, 'UTF-8')?></title>
+<title><?php echo(htmlspecialchars($session_id, ENT_QUOTES, 'UTF-8'))?></title>
 <meta charset="UTF-8">
 
 
 <body>
-  <?php echo $skim_str ?>
+  <?php echo($skim_str) ?>
 </body>
 
 </html>
